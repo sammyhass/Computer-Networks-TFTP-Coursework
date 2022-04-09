ec37a3da7d57e859469a0da3f66adabdab65a5e7
Sammy Hass <sammyhass01@gmail.com>
2022-04-09 16:08:16 +0100

    started on tcp server and client

--- TFTP-TCP-Client/src/main/java/App.java
+++ TFTP-TCP-Client/src/main/java/App.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class App {
-    Socket socket;
     public static void main(String[] args) throws TFTPException, IOException {
         Socket socket = new Socket("localhost", 8080);
         TFTPClient client = new TFTPClient(socket);
